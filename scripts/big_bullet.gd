29a1176165bb08fc99e2bc66e8a09b47227c9746
breadlofe <98194084+breadlofe@users.noreply.github.com>
2024-02-08 10:28:15 -0500

    bullet charge done

--- scripts/big_bullet.gd
+++ scripts/big_bullet.gd
@@ -1,16 +1,17 @@
 extends Area2D
-@export var speed: int = 200
-var dir: int
 
+@export var speed: int = 200
+@onready var _animation_player = $AnimationPlayer
 # Called when the node enters the scene tree for the first time.
+var dir: int
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	_animation_player.play("fire")
 	translate(Vector2(speed*delta*dir,0))
-	#self.postion.x += delta
 
 func _set_dir(facing):
 	dir = facing
--- scripts/character.gd
+++ scripts/character.gd
@@ -4,25 +4,36 @@ extends CharacterBody2D
 @export  var SPEED = 300.0
 @export  var JUMP_VELOCITY = -400.0
 @export  var BULLET: PackedScene
+@export  var BIG_BULLET: PackedScene
+@export  var SHOT_TIMER: float = 1.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var _animation_player = $AnimationPlayer
 @onready var _sprite_2D = $Sprite2D
+var dir: int = 1
 
 func _process(_delta):
 	if Input.is_action_pressed("move_right"):
 		_animation_player.play("walk")
 		_sprite_2D.flip_h = false
+		dir = 1
 	elif Input.is_action_pressed("move_left"):
 		_animation_player.play("walk")
 		_sprite_2D.flip_h = true
+		dir = -1
 	else:
 		_animation_player.stop()
 	if Input.is_action_pressed("shoot"):
+		SHOT_TIMER -= _delta
+		$shot_charge.modulate.a = 1.0 - SHOT_TIMER
 		$shot_charge.show()
 	if Input.is_action_just_released("shoot"):
-		spawn_bullet(self.position, self.rotation, $bullet_holder.global_position)
+		if SHOT_TIMER > 0:
+			spawn_bullet($bullet_holder.global_position, self.rotation, 0)
+		else:
+			spawn_bullet($bullet_holder.global_position, self.rotation, 1)
+		SHOT_TIMER = 1.0
 		$shot_charge.hide()
 
 func _physics_process(delta):
@@ -45,10 +56,13 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
-func spawn_bullet(pos, rot, test):
-	print("WAH", pos)
-	print("TEST", test)
-	var bullet = BULLET.instantiate()
-	bullet.position = test
+func spawn_bullet(pos, rot, type):
+	var bullet 
+	if type == 0:
+		bullet = BULLET.instantiate()
+	else:
+		bullet = BIG_BULLET.instantiate()
+	bullet._set_dir(dir)
+	bullet.position = pos
 	bullet.rotation = self.rotation
 	get_tree().get_root().add_child(bullet)
