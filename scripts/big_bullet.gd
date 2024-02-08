c417953f5a5e1a162c93a3ceb3f25cb65bc51ba2
breadlofe <98194084+breadlofe@users.noreply.github.com>
2024-02-08 18:30:15 -0500

    Evil smog and mask test

--- scripts/big_bullet.gd
+++ scripts/big_bullet.gd
@@ -2,12 +2,13 @@ extends Area2D
 
 @export var speed: int = 200
 @onready var _animation_player = $AnimationPlayer
-@onready var particle_emitter = get_node("/root/ParticleController")
+#@onready var particle_emitter = get_node("/root/ParticleController")
 @onready var big_shot_explosion = preload("res://scenes/bullet_explosion.tscn")
 #signal emit_particles(particle, position, rotation)
 #var big_shot_explosion = preload("res://scenes/bullet_explosion.tscn")
 # Called when the node enters the scene tree for the first time.
 var dir: int
+@onready var TimeCreated = Time.get_ticks_msec()
 func _ready():
 	pass # Replace with function body.
 
@@ -16,6 +17,8 @@ func _ready():
 func _process(delta):
 	_animation_player.play("fire")
 	translate(Vector2(speed*delta*dir,0))
+	if Time.get_ticks_msec() - TimeCreated > 5000:
+		queue_free()
 
 func _set_dir(facing):
 	dir = facing
@@ -24,7 +27,7 @@ func _set_dir(facing):
 func _on_body_entered(body):
 	if body.is_in_group("enemy"):
 		body.got_hit(1)
-		particle_emitter.emit(get_node("/root/Game/Level1"), "big_shot_explosion", self.global_position, self.rotation)
+		#particle_emitter.emit(get_node("/root/Game/Level1"), "big_shot_explosion", self.global_position, self.rotation)
 		#emit_particles.emit(big_shot_explosion, self.position, self.rotation, true)
 		var temp_var = big_shot_explosion.instantiate()
 		temp_var.global_position = self.global_position
--- scripts/bullet_explosion.gd
+++ scripts/bullet_explosion.gd
@@ -1,6 +1,5 @@
-extends Area2D
-@export var speed: int = 200
-var dir: int
+extends GPUParticles2D
+
 @onready var TimeCreated = Time.get_ticks_msec()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,15 +8,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	translate(Vector2(speed*delta*dir,0))
 	if Time.get_ticks_msec() - TimeCreated > 1000:
 		queue_free()
-
-func _set_dir(facing):
-	dir = facing
-
-
-func _on_body_entered(body):
-	if body.is_in_group("enemy"):
-		body.got_hit(0)
-		self.queue_free()
